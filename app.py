d024579e154948f45908915b8af78ddb259cd4b4
Oushnik20 <137035839+Oushnik20@users.noreply.github.com>
2025-12-06 22:57:01 +0530

    use port in deploy

--- app.py
+++ app.py
@@ -139,6 +139,7 @@ def extract_score(text):
                 continue
     return 7.5
 
-
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port, debug=True)
 if __name__ == "__main__":
     app.run(debug=True)
